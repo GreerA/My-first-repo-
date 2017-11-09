7d0d31b074be11fbdb996a1e226713e66a344be3
GreerA <33520745+GreerA@users.noreply.github.com>
2017-11-09 09:53:20 -0500

    Initial

---
+++ Mad Libs.py
@@ -0,0 +1,52 @@
+### MAD LIBS ###
+### Source: http://www.woojr.com/wp-content/uploads/2010/02/spring-mad-libs-bicycle.jpg
+
+
+print("Write a verb/ing")
+verbing1 = input()
+
+print("Write an adjective")
+adjective2 = input()
+
+print("Write another verb/ing")
+verbing3 = input()
+
+print("Write a Bodypart")
+bodypart4 = input()
+
+print("Write an Adverb")
+adverb5 = input()
+
+print ("Write a bodypart")
+bodypart6 = input()
+
+print ("Write a Noun")
+noun7 = input()
+
+print ("Write a Verb")
+verb8 = input()
+
+print ("Animal")
+animal9 = input()
+
+print ("Write a noun")
+noun10 = input()
+
+print ("Write a verb")
+verb11 = input()
+
+print ("Write an adjective")
+adjective12 = input()
+
+print ("Write a color")
+color13 = input() 
+
+### MAD LIB ###
+
+print(" Most doctors agree that bicycle " + verbing1 +
+      " is a/n " + adjective2 + " form of exercise. " + verbing3 + " a bicyle enables you to develope your "
+      + bodypart4 + " muscles as well as " + adverb5 + " increase the rate of your " +
+      bodypart6 + " beat. More " + noun7 + " around the world " + verb8 +
+      " bicyles than drive " + animal9 + " Nomatter what kind of " +
+      noun10 + " you " + verb11 + " , always be sure to wear a/an " + adjective12 +
+      " helmet. Make sure to have " + color13 + " reflectors too! ")
---
+++ Personality_GA.py
@@ -0,0 +1,24 @@
+name = "Greer"
+state = "California"
+city = "San Francisco"
+book = "The Giver"
+tvshow = "How I met your mother"
+sport1 = "swim"
+sport2 = "soccer"
+
+
+
+print (name + " likes to visit " + city + " , " + state + ".")
+
+print ( name + " likes to watch " + tvshow + " and likes to read " + book)
+
+print ( name + "likes to" + sport1 + " and play " + sport2)
+
+print ("What's your favorite color?")
+color = input ()
+if color == " blue ":
+    print (" I like blue too!")
+ else:
+     print (" is nice too, but I like blue ")
+        
+
